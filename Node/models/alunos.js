f4594900494eac8ffbf6da7a08912d1a0223611d
WelberSilverio <welbersilverio@gmail.com>
2018-02-13 20:07:56 -0200

    Fechar conexao

--- Node/models/alunos.js
+++ Node/models/alunos.js
@@ -5,6 +5,7 @@ function aluno(connection) {
 aluno.prototype.cadastro = function(data, callback) {
 	console.log(data);
 	this._connection.query('INSERT INTO alunos SET ?',data, callback);
+	this._connection.end();
 };
 
 module.exports = function(app) {
--- Node/models/usuarios.js
+++ Node/models/usuarios.js
@@ -4,6 +4,7 @@ function usuarios(connection) {
 
 usuarios.prototype.login = function(data, callback) {	
 	this._connection.query('SELECT 1 FROM CAD_USER WHERE USER = ? AND SENHA = ?', data, callback);
+	this._connection.end();
 };
 
 module.exports = function(app) {
