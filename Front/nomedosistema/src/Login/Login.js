6a042f2a3210921388d2382f8c59d86bc89cd7c4
WelberSilverio <welbersilverio@gmail.com>
2018-02-27 16:53:17 -0300

    Design Login

--- Front/nomedosistema/src/Login/Login.js
+++ Front/nomedosistema/src/Login/Login.js
@@ -5,21 +5,25 @@ class MenuLogin extends Component {
   render() {
     return (
       <div className="menu-login">
-        <div class="card grey lighten-2">
-          <form>
-            <div className="input-field col s3">
-              <input id="usuario" type="text" className="validate " />
-              <label for="usuario" className="blue-text text-lighten-2 center-align">Usuario</label>
+        <div className="z-depth-4 grey lighten-4 row">
+          <form className="col s12" method="get">
+            <div className="Row">
+              <div className="input-field col s8 offset-s2">
+                <input id="usuario" type="text" className="validate " />
+                <label for="usuario" className="blue-text text-lighten-2 center-align">Usuario</label>
+              </div>
             </div>
-            <div className="input-field col s3">
-              <input id="senha" type="password" className="validate " />
-              <label for="senha" className="blue-text text-lighten-2 center-align">Senha</label>
+            <div className="Row ">
+              <div className="input-field col s8 offset-s2">
+                <input id="senha" type="password" className="validate " />
+                <label for="senha" className="blue-text text-lighten-2 center-align">Senha</label>
+              </div>
             </div>
-            
-              <button class="btn waves-effect waves-light blue lighten-2" type="submit" name="action">Entrar
+
+            <button className="btn waves-effect waves-light blue lighten-2" type="submit" name="action">Entrar
               <i class="material-icons right">send</i>
-              </button>
-           
+            </button>
+
           </form>
         </div>
       </div>
