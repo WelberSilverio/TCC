be5c669087d160c8a4149dd36c2d546a5bbdbfed
WelberSilverio <welbersilverio@gmail.com>
2018-02-28 16:27:34 -0300

    Criacao de lista na tabela

--- Front/nomedosistema/src/Tabelas/table.js
+++ Front/nomedosistema/src/Tabelas/table.js
@@ -3,16 +3,26 @@ import './table.css';
 
 class Table extends Component {
     render() {
+
+        let titulos = [
+            {colname:"Teste",colvalor:"21"},
+            {colname:"Teste1",colvalor:"22"},
+            {colname:"Teste2",colvalor:"23"}
+        ];
+
+        let litatitulos = titulos.map(function(valor){
+            return(
+                <th key={valor.colvalor}>{valor.colname}</th>  
+            );
+        });
+
         return (
             <div className="row">
                 <div className="col s12 m12 l6 offset-l2 center">
                     <table cellpadding="1" cellspacing="1" className="table table-hover responsive-table bordered" id="myTable">
                         <thead>
                             <tr>
-                                <th>Coluna Title 1</th>
-                                <th>Coluna Title 2</th>
-                                <th>Coluna Title 3</th>
-                                <th>Coluna Title 4</th>
+                                {litatitulos}
                             </tr>
                         </thead>
                         <tbody >
