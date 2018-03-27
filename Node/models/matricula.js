a785f0ae583e8fe0efc5a1ea2e3071d6182007f6
WelberSilverio <welbersilverio@gmail.com>
2018-03-27 16:47:01 -0300

    Mudancas em Matricula

--- Node/models/matricula.js
+++ Node/models/matricula.js
@@ -1,23 +1,3 @@
-function matricula(connection) {
-	this._connection = connection;
-};
-
-matricula.prototype.cadastro = function (data, callback) {
-	this._connection.query('INSERT INTO MATRICULAS SET ?', data, callback);
-	this._connection.end();
-};
-
-matricula.prototype.confirma = function (data, callback) {
-	this._connection.query('UPDATE MATRICULAS SET ?', data, callback);
-	this._connection.end();
-};
-
-matricula.prototype.seleciona = function (data, callback) {
-	//ALTERAR SELECT PARA SELECIONAR TUDO MATRICULA E NOME, TELEFONE, CELULAR (Disponibilidade de hor�rio tbm, ou em outro lugar)
-	this._connection.query('SELECT * FROM MATRICULAS WHERE ?', data, callback);
-	this._connection.end();
-};
-
 module.exports = (DataType, sequelize) => {
 	var Matricula = sequelize.define("Matricula", {
 		id_matricula: {
--- Node/routes/matricula.js
+++ Node/routes/matricula.js
@@ -9,11 +9,27 @@ module.exports = (app) => {
 	});
 
 	app.put('/matriculas/confirma', function (req, res) {
-
+		Matricula.update(req.body, { where: req.params })
+			.then(result => res.sendStatus(204))
+			.catch(error => {
+				res.status(412).json({ msg: error.message });
+			});
 	});
 
 	app.get('/matriculas/selecionaconfirma', function (req, res) {
-
+		//ALTERAR SELECT PARA SELECIONAR TUDO MATRICULA E NOME, TELEFONE, CELULAR (Disponibilidade de horário tbm, ou em outro lugar)
+		Matricula.findAll({ where: req.params, include: [Aluno] })
+			.then(result => {
+				if (result) {
+					res.json(result);
+				}
+				else {
+					res.sendStatus(404);
+				}
+			})
+			.catch(error => {
+				res.status(412).json({ msg: error.message });
+			});
 	});
 
 }
