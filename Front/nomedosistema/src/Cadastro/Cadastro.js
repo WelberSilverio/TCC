0a8afdcbc747d7a2a49e1f638b00ffc86b1b7006
WelberSilverio <welbersilverio@gmail.com>
2018-04-26 11:31:09 -0300

    Criação de estilos e Mudancas Cadastro

--- Front/nomedosistema/src/Cadastro/Cadastro.js
+++ Front/nomedosistema/src/Cadastro/Cadastro.js
@@ -4,37 +4,53 @@ import { Route, NavLink, HashRouter } from 'react-router-dom'
 class Cadastro extends Component {
     render() {
         return (
-            <form className="col s12">
-                <div className="z-depth-4 grey lighten-4 row">
-                    <div className="Row">
-                        <div className="input-field col s7 offset-s2">
-                            <input id="CPF" type="text" className="" />
-                            <label for="CPF" className="blue-text text-lighten-2 center-align">CPF</label>
+            <div class="menu-conteudo">
+                <form className="col s12">
+                    <div className="z-depth-4 grey lighten-4 row">
+                        <div className="Row">
+                            <div className="input-field col s7 offset-s2">
+                                <input id="CPF" type="text" className="" />
+                                <label for="CPF" className="blue-text text-lighten-2 center-align">CPF</label>
+                            </div>
                         </div>
+                        <div className="Row">
+                            <div className="input-field col s7 offset-s2">
+                                <input id="Nome" type="text" className="" />
+                                <label for="Nome" className="blue-text text-lighten-2 center-align">Nome</label>
+                            </div>
+                        </div>
+                        <div className="Row">
+                            <div className="input-field col s4 offset-s2">
+                                <input id="dtNasc" type="text" class="datepicker" data-mask="00/00/0000" />
+                                <label for="dtNasc" className="blue-text text-lighten-2 center-align">Data de Nascimento</label>
+                            </div>
+                            <div className="input-field col s3">
+                                <select>
+                                    <option value="" disabled selected>Selecione:</option>
+                                    <option value="Mas">Masculino</option>
+                                    <option value="Fem">Feminino</option>
+                                </select>
+                                <label>Sexo</label>
+                            </div>
+                        </div>
+                        <div className="Row">
+                            <div className="input-field col s4 offset-s1">
+                                <input id="RG" type="text" className="" />
+                                <label for="RG" className="blue-text text-lighten-2 center-align">RG</label>
+                            </div>
+                            <div className="input-field col s3">
+                                <input id="orgExpedidor" type="text" className="" />
+                                <label for="orgExpedidor" className="blue-text text-lighten-2 center-align">Orgão Expedidor</label>
+                            </div>
+                            <div className="input-field col s3">
+                                <input id="estado" type="text" className="" />
+                                <label for="estado" className="blue-text text-lighten-2 center-align">Estado</label>
+                            </div>
+                        </div>
+                        <button className="btn waves-effect waves-light blue lighten-2" type="submit" name="action">Finalizar Cadastro</button>
                     </div>
-                    <div className="Row">
-                        <div className="input-field col s7 offset-s2">
-                            <input id="Nome" type="text" className="" />
-                            <label for="Nome" className="blue-text text-lighten-2 center-align">Nome</label>
-                        </div>
-                    </div>
-                    <div className="Row">
-                        <div className="input-field col s4 offset-s2">
-                            <input id="dtNasc" type="text" className="" />
-                            <label for="dtNasc" className="blue-text text-lighten-2 center-align">Data de Nascimento</label>
-                        </div>
-                        <div className="input-field col s4 offset-s2">
-                            <select>
-                                <option value="" disabled selected>Selecione:</option>
-                                <option value="Mas">Masculino</option>
-                                <option value="Fem">Feminino</option>
-                            </select>
-                            <label>Sexo</label>
-                        </div>
-                    </div>
-                    <button className="btn waves-effect waves-light blue lighten-2" type="submit" name="action">Finalizar Cadastro</button>
-                </div>
-            </form>
+                </form>
+            </div>
         );
     }
 }
--- Front/nomedosistema/src/index.js
+++ Front/nomedosistema/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Main from './Main/Main';
+import MenuLogin from './Login/Login';
+import Cadastro from './Cadastro/Cadastro';
 
-ReactDOM.render(<Main/>, document.getElementById('root'));
+ReactDOM.render(<Cadastro/>, document.getElementById('root'));
 registerServiceWorker();
